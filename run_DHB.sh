2ea8da847ffd271fddd97c50b526371913283846
winpaw1n <98765278+winpaw1n@users.noreply.github.com>
2025-05-08 14:14:29 -0500

    Add files via upload

--- run_DHB.sh
+++ run_DHB.sh
@@ -4,9 +4,10 @@ CUDA_VISIBLE_DEVICES=0 python main.py  \
 --dataset_path ./data/DHB-dataset \
 --scenes_list ./data/DHB_scene_list_test.txt \
 --num_points 1024 \
---interval 4 \
+--interval 1 \
 --iters 1000 \
 --lr 0.001 \
+--num_frames 4 \
 --layer_width 512 \
 --act_fn LeakyReLU \
 > .log_DHB_release_test 2>&1
